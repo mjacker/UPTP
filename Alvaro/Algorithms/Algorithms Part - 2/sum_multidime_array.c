843fcf8352932920f93c1469dfda981c04ff7e41
alvaroroque.jaragaleano.uptp@gmail.com <alvaroroquejaragaleano00@gmail.com>
2021-06-01 12:25:32 -0400

    Add new files of PL9

---
+++ Alvaro/Algorithms/Algorithms Part - 2/sum_multidime_array.c
@@ -0,0 +1,28 @@
+ #include<stdio.h>
+ #define LEN 6
+ int sum_two_dim_array(int a[ ][LEN], int n)
+ {
+ 	int i,j,sum=0;
+ 	
+ 	for(i = 0; i < n; i++)
+ 		for(j = 0; j < LEN; j++)
+ 			sum +=a[i][j];
+ 	
+ 	return sum;
+ }
+ 
+ int main(void)
+ {
+ 	int array[4][6]={{1, 2, 3, 4, 5, 6},
+ 					{7, 8, 9, 10, 11, 12},
+ 					{13, 14, 15, 16, 17, 18},
+ 					{19, 20, 21, 22, 23, 24}};
+ 					
+
+ 	int total =0;
+ 	total = sum_two_dim_array(array, 4);
+ 	printf("%d \n", total);
+ 	
+ 	return 0;
+	 	
+ }
