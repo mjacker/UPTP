57f8c3c51fabeba1033fb38636c34724700c9009
Jaime Colman <mjackerjaime@gmail.com>
2021-06-21 09:55:32 -0400

    Usar funciones con punteros como parametros. tiene alertas de advertencia.

---
+++ Jaime/PL 011 Pointers/11-4 Pointers as Arguments.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+void max_min(int Array[], int n, int *max, int *min);
+
+int main(void){
+	int vector [] = {34, 82, 49, 102, 7, 94, 23, 11, 50, 31};
+	
+	int maximo;
+	int minimo;
+	int N =  (sizeof(vector)/sizeof(vector[0]));
+	printf("N: %d", N);
+	
+	max_min(vector, N, &maximo, &minimo);
+	
+	printf("\nLargest: %d\nSmallest: %d", maximo, minimo);
+	
+	return 0;
+}
+
+void max_min(int Array[], int n, int *max, int *min){
+	int i;
+	*max = Array[0];
+	*min = Array[0];
+	for(i = 0; i < n; i++)
+	{
+		Array[i] > *max ? *max = Array[i] : "" ;
+		Array[i] < *min ? *min = Array[i] : "" ;
+	}
+}
